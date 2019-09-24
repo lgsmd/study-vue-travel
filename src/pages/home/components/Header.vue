96f834c36ab90893d4958f3cd65f3ab252b7a30c
lgsmd <584588192@qq.com>
2019-09-24 18:37:18 +0800

    add icon in Header

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="header">
-    <div class="header-left">返回</div>
-    <div class="header-input">输入城市/景点/游玩主题</div>
-    <div class="header-right">地址</div>
+    <div class="header-left">
+      <div class="iconfont back-icon">&#xe658;</div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe63c;</span>
+      输入城市/景点/游玩主题
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont arrow-icon">&#xe65c;</span>
+    </div>
   </div>
 </template>
 
@@ -13,25 +21,33 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+@import "~styles/varibles.styl"
 .header
+  height: .86rem
   line-height: .86rem
-  background-color: #40d8e4
+  background-color: $bgColor
   display: flex
   color : #fff
   .header-left
     width: .64rem
     float: left
+    .back-icon
+      text-align: center
+      font-size: .6rem
   .header-input
     flex: 1
     color: #cacaca
     background: #fff
     height: .62rem
     line-height: .62rem
+    padding-left: .2rem
     margin-top: .12rem
     margin-left: .2rem
     border-radius: .1rem
   .header-right
-    width: 1.24rem
+    width: 1.28rem
     float: right
     text-align: center
+    .arrow-icon
+      margin-left: -.1rem
 </style>
