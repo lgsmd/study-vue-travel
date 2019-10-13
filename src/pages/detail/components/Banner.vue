379584754e02ac5db54dccbbb90570f2ff638bb5
lgsmd <584588192@qq.com>
2019-10-13 20:02:22 +0800

    fix problem

--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <div class="banner" @click="handleBannerClick">
+    <div
+      class="banner"
+      @click="handleBannerClick"
+    >
       <img class="banner-img"
            :src="bannerImg"/>
       <div class="banner-info">
